edc16a2dce8c38dbd936a0069b3b2b5f64b675f0
alexsandro-silva <alexsandrorodriguess@gmail.com>
2023-09-08 01:31:06 -0300

    Primeiro commit

---
+++ java-api/src/test/java/br/com/arsilva/vendormanager/javaapi/JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arsilva.vendormanager.javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
