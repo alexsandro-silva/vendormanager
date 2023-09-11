5543079ffa2c064b7581973463d789268e4d40c3
Alexsandro Rodrigues <alexsandrorodriguess@gmail.com>
2023-09-11 00:57:28 -0300

    Implementação da classe Fornecedor

--- java-api/src/main/java/br/com/arsilva/vendormanager/javaapi/models/Fornecedor.java
+++ java-api/src/main/java/br/com/arsilva/vendormanager/javaapi/models/Fornecedor.java
@@ -1,2 +1,27 @@
-package br.com.arsilva.vendormanager.javaapi.models;public class Fornecedor {
+package br.com.arsilva.vendormanager.javaapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+public class Fornecedor {
+    @Id
+    private String cpfCnpj;
+    private String nome;
+    private String email;
+    private String rg;
+    private Date dataNascimento;
+    @Embedded
+    private Endereco endereco;
 }
